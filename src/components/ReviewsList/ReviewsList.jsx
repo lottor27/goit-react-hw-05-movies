6b8f957b646042a3904628b97589b40bd8235af6
lottor27 <120725846+lottor27@users.noreply.github.com>
2023-08-13 23:18:51 +0300

    nu finish blin

--- src/components/ReviewsList/ReviewsList.jsx
+++ src/components/ReviewsList/ReviewsList.jsx
@@ -1,23 +1,39 @@
+import { nanoid } from 'nanoid'
 import React from 'react'
 
+
 const ReviewsList = ({ reviewsResults }) => {
+  
+
+
     return (
         <div className="accordion" id="accordionExample" >
-            {reviewsResults.map(review => {
+        {reviewsResults.map(review => {
+          const colapsId = nanoid();
+            const check = `#${colapsId}`;
                 return (
-                    <div className="accordion-item" key={review.id}>
-    <h2 className="accordion-header">
-      <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-        {review.author}
-      </button>
-    </h2>
-    <div id="collapseOne" className="accordion-collapse collapse show" data-bs-parent="#accordionExample">
-      <div className="accordion-body">
-        {review.content}
-      </div>
-    </div>
-    </div>
-                )
+                  <div className="accordion-item" key={review.id}>
+                    <h2 className="accordion-header">
+                      <button
+                        className="accordion-button"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target={check}
+                        aria-expanded="true"
+                        aria-controls={colapsId}
+                      >
+                        {review.author}
+                      </button>
+                    </h2>
+                    <div
+                      id={colapsId}
+                      className="accordion-collapse collapse hide"
+                      data-bs-parent="#accordionExample"
+                    >
+                      <div className="accordion-body">{review.content}</div>
+                    </div>
+                  </div>
+                );
             })}
         </div>
     )
